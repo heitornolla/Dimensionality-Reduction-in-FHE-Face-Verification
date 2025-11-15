6bf4c000ad7a316996e9729c2e16bd6914022325
heitor <heitor.nolla@gmail.com>
2025-11-15 12:44:51 -0300

    add code to reproduce results

---
+++ scripts/reproduce_paper_results.py
@@ -0,0 +1,28 @@
+from baseline_verification import main as baseline
+from fhe_baseline import main as fhe_baseline
+from fhe_jl import main as jl_reduction
+from fhe_learned_autoencoder import main as ae_reduction
+from fhe_pca import main as pca_reduction
+from fhe_rand_proj_gauss import main as grp_reduction
+from fhe_rand_proj_sparse import main as srp_reduction
+from fhe_rsvd import main as rsvd_reduction
+
+
+if __name__ == "__main__":
+    baseline()
+    fhe_baseline(csv_path='results/baseline')
+
+    for i in range(10):
+        print(f'--- RUN {i+1} ---\n')
+        print('\nRunning JL\n')
+        jl_reduction(csv_path='results/jl_results')
+        print('\nRunning AE\n')
+        ae_reduction(csv_path='results/ae_results')
+        print('\nRunning PCA\n')
+        pca_reduction(csv_path='results/pca_results')
+        print('\nRunning GRP\n')
+        grp_reduction(csv_path='results/grp_results')
+        print('\nRunning SRP\n')
+        srp_reduction(csv_path='results/srp_results')
+        print('\nRunning RSVD\n')
+        rsvd_reduction(csv_path='results/rsvd_results')
